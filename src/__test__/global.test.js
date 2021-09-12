017babd7ecc13d1f02d2beae5e79706e9a035e51
Carlos Moran <cmoran@applaudostudios.com>
2021-09-12 17:11:25 -0600

    First commit

---
+++ src/__test__/global.test.js
@@ -0,0 +1,49 @@
+const text = "Hola Mundo";
+const fruits = ['apple', 'melon', 'banana'];
+
+test('Debe contener un text', () => {
+  expect(text).toMatch(/Mundo/);
+});
+
+test('Debe contener una banana', () => {
+  expect(fruits).toContain('banana');
+});
+
+test('Debe ser mayor que "9"', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('Debe ser verdadero', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split('').reverse().join(''));
+};
+
+test('Probar el callback', () => {
+  reverseString('Hola', (str) => {
+    expect(str).toBe('aloH');
+  });
+});
+
+const reverseStringAsync = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error('Error!'));
+    }
+    resolve(str.split('').reverse().join(''));
+  });
+};
+
+test('Probando la promesa', () => {
+  reverseStringAsync('Hola')
+    .then(str => {
+      expect(str).toBe('aloH');
+    });
+});
+
+test('Probando async/await', async () => {
+  const str = await reverseStringAsync('hola');
+  expect(str).toBe('aloh');
+});
