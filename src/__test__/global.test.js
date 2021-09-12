980bb6e48cfbd7f501466e31118268f1f80264b5
Carlos Moran <cmoran@applaudostudios.com>
2021-09-12 17:17:41 -0600

    Code before/after tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -47,3 +47,9 @@ test('Probando async/await', async () => {
   const str = await reverseStringAsync('hola');
   expect(str).toBe('aloh');
 });
+
+afterEach(() => console.log('Después de cada prueba'));
+afterAll(() => console.log('Después de todas las pruebas'));
+
+beforeEach(() => console.log('Antes de cada prueba'));
+beforeAll(() => console.log('Antes de todas las pruebas'));
