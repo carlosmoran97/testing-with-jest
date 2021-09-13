16a1f55f2034893e168cdd39bf4950d9aa816c06
Carlos Moran <cmoran@applaudostudios.com>
2021-09-12 21:01:13 -0600

    testing coverage command

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ const randomString = () => {
   return string;
 };
 
+
+const reverseStringAsync = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error('Error!'));
+    }
+    resolve(str.split('').reverse().join(''));
+  });
+};
+
+
 module.exports = randomString;
